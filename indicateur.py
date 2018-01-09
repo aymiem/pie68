3d2085bddb8551df55852aa0d0baa3b05024b3a4
slanino <laninosebastien@gmail.com>
2018-01-09 16:01:02 +0100

    je rajoute le fichier python indicateur

---
+++ indicateur.py
@@ -0,0 +1,35 @@
+from objects import *
+from algorithme import *
+import pandas as pd
+import numpy as np
+from lecture import *
+from ecriture import *
+from constantes import *
+
+
+def Init_Indicateurs(d):
+    
+    MpotH = dict()
+    for a in d["listeAvion"]:
+        nomAvion = a.nom
+        MpotH[nomAvion] = [] #On crée une liste vide pour chaque avion
+    MpotH["somme"] = [] * ( d["temps"] - 3 ) #On crée une liste qui sera la somme des pot. sur chaque période
+
+    NbrMaint = [] * ( d["temps"] - 3 )
+    
+    return (MpotH, NbrMaint)
+    
+    
+def Remplir_Indicateurs(d, df, MpotH, NbrMaint, t):
+    for a in d["listeAvion"]:
+        nomAvion = a.nom
+        MpotH(nomAvion).append(a.pot_horaire) # On rajoute la dernière valeur à la liste
+    
+    for a in d["listeAvion"]:
+        MpotH["somme"][t] += MpotH[a][t] # On fait la somme des pot. sur tous les avions à chaque période
+        
+    for a in d["listeAvion"]:
+        if str(df.xs(t)[a])[0] == "V":
+            NbrMaint[t] += 1
+            
+
