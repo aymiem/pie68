c385684dae65399c764a4576ed46bdcbe057d663
mfoulgoc <melvin.foulgoc@supaero.isae.fr>
2018-01-23 17:03:48 +0100

    ajout indicateurs sur pot perdu (demande nicolas)

--- ecriture.py
+++ ecriture.py
@@ -20,7 +20,9 @@ def ecriture_donnees(listMission,indic,nom_fichier):
         spamwriter.writerow(['var_maint'] + [indic["Maint_var"]]) #variance maint, à minimiser
         spamwriter.writerow(['max_maint'] + [indic["Max_maint"]]) #max maint, contrainte de capacité à ne pas dépasser
         spamwriter.writerow(['pot_cal_tot'] + [indic["PotCalTot"]]) #Disponibilité totale planifié, soit le nombre total de créneau moins le nombre de créneau occupé par une maintenance
-        spamwriter.writerow(['pot_perdu'] + [indic["PotPerdu"]]) #Potentiel perdu à minimiser
+        spamwriter.writerow(['pot_perdu'] + [indic["PotPerdu"]]) #Potentiel perdu total à minimiser
+        spamwriter.writerow(['var_pot_perdu'] + [indic["varPotPerdu"]]) #Potentiel perdu minimum 
+        spamwriter.writerow(['moy_pot_perdu'] + [indic["moyPotPerdu"]]) #Potentiel perdu moyen
         spamwriter.writerow(['min_dispo'] + [indic["min_dispo"]]) # nbr d'avion dispo (mission + entrainement), à maximiser
         
         for m in listMission:
