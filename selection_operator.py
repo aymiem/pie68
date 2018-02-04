aa362fd2568959dbb28e83d8592e138c876cbb68
mfoulgoc <melvin.foulgoc@supaero.isae.fr>
2018-02-04 23:17:31 +0100

    new indics et fitness objectif

--- selection_operator.py
+++ selection_operator.py
@@ -13,14 +13,29 @@ def evaluation_fitness(df_classement):
     # on récupère ici les poids des indicateurs
     df_poids = pd.read_csv('poids_indicateurs.csv',sep=";",header=0,index_col=0)
 
-    df_RWS["fitness"] = np.zeros(len(df_RWS))
+
+    df_obj1 = df_RWS.loc[:,['var_maint','var_maint_rg','delta_nbmaint','delta_nbmaint_rg']]
+    df_obj2 = df_RWS.loc[:,['min_pot_perdu','moy_pot_perdu','min_pot_perdu_rg','moy_pot_perdu_rg']]
+    
+    
+    df_obj1["fitness_obj_1"] = np.zeros(len(df_obj1))
+    df_obj2["fitness_obj_2"] = np.zeros(len(df_obj2))
+    print(df_obj1)
+    print(df_obj2)
+    # somme pondérée des scores pour obj1
+    for nom_indic in ['var_maint','delta_nbmaint']:
+        df_obj1["fitness_obj_1"] = df_obj1["fitness_obj_1"] + np.asarray(df_obj1[nom_indic+"_rg"])*(df_poids.loc[nom_indic,"poids"])
+    
+    for nom_indic in ['min_pot_perdu','moy_pot_perdu']:
+        df_obj2["fitness_obj_2"] = df_obj2["fitness_obj_2"] + np.asarray(df_obj2[nom_indic+"_rg"])*(df_poids.loc[nom_indic,"poids"])
+    
     
     # somme pondérée des scores par indicateurs
     for nom_indic in list(df_poids.index):
         df_RWS["fitness"] = df_RWS["fitness"] + np.asarray(df_RWS[nom_indic+"_rg"])*(df_poids.loc[nom_indic,"poids"])
     
-    print(df_RWS)
-    return df_RWS.sort_values(by=["fitness"], ascending=False)
+    
+    return df_obj1.sort_values(by=["fitness_obj_1"], ascending=False)
 
 
 def Roulette_wheel_selection(df_classement, N): 
